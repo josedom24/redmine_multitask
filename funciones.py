f446b15f402097364d8ed3d8b5d5b861eb2728b4
Jose Domingo <jose@toshibita>
2016-09-16 23:58:59 +0200

    Borrado·

--- funciones.py
+++
@@ -1,9 +0,0 @@
-import time
-def validateDateEs(date):
-    for format in ['%d/%m/%Y', '%d/%m/%y', '%d/%m/%Y %H:%M:%S', '%d/%m/%y %H:%M:%S']:
-        try:
-            result = time.strptime(date, format)
-            return True
-        except:
-            pass
-    return False
